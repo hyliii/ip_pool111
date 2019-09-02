202e93503e687cba8e885b647bf67e0b1cedbdd6
苏振江 <krissu@163.com>
2019-09-02 01:39:02 +0000

    Initial commit

---
+++ api/apiServer.py
@@ -0,0 +1,32 @@
+# coding:utf-8
+import json
+import sys
+import web
+import config
+from db.DataStore import sqlhelper
+urls = (
+    '/', 'select',
+    '/delete', 'delete',
+)
+def start_api_server():
+    sys.argv.append('0.0.0.0:%s' % config.API_PORT)
+    app = web.application(urls, globals())
+    app.run()
+class select(object):
+    def GET(self):
+        inputs = web.input()
+        json_result = json.dumps(sqlhelper.select(inputs.get('count', None), inputs))
+        print('select'+str(json_result))
+        return json_result
+class delete(object):
+    params = {}
+    def GET(self):
+        inputs = web.input()
+        json_result = json.dumps(sqlhelper.delete(inputs))
+        print('delect' + str(json_result))
+        return json_result
+if __name__ == '__main__':
+    sys.argv.append('0.0.0.0:8000')
+    app = web.application(urls, globals())
+    app.run()
+    # start_api_server()
---
+++ util/logger.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+import logging
+logger = logging.getLogger()
+
+
+def logger_proxy(proxy):
+    logger.setLevel(logging.INFO)
+    logger.info(proxy)
