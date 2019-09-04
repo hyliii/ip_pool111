ffd162a8b9c31713e5184872761feffb460df1cb
hyl <1633487955@qq.com>
2019-09-04 12:52:31 +0800

    功能函数更新

--- spider/HtmlPraser.py
+++ spider/HtmlPraser.py
@@ -113,7 +113,7 @@ class Html_Parser(object):
                     t_way = 0
                     protocol = 0
                     addr = self.ips.getIpAddr(self.ips.str2ip(ip))
-                    t_service = addr[-2:]
+                    t_service = self.checkservice(addr)
                     country,addr=self.addrcut(addr)
                 except Exception as e:
                     continue
